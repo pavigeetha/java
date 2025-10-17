2a74fc8c84a169c41d7f2b0ef4dd84c7d03c5e6b
pavigeetha <pavi.ramesh2019@gmail.com>
2025-10-17 09:42:06 +0530

    Add files via upload

--- Exc.java
+++ Exc.java
@@ -27,8 +27,7 @@ class InsufficientBalanceException extends Exception {
 
 public class Exc {
 
-    static void dynamicArrayDemo() {
-        Scanner sc = new Scanner(System.in);
+    static void dynamicArrayDemo(Scanner sc,PrintWriter log) {
         try {
             System.out.print("\nEnter array size: ");
             int size = sc.nextInt();
@@ -39,16 +38,19 @@ public class Exc {
 
             int[] arr = new int[size];
             System.out.println("Array of size " + size + " created successfully!");
+            log.println("Array of size " + size + " created successfully!");
         } 
         catch (InvalidArraySizeException e) {
             System.out.println("Custom Exception: " + e.getMessage());
+            log.println("Custom Exception: \" + e.getMessage()");
         } 
         catch (InputMismatchException e) {
             System.out.println("Input Error: Please enter an integer.");
+            log.println("Input Error: Please enter an integer.");
         } 
         finally {
             System.out.println("Array size validation complete(finally block) .\n");
-            sc.close();
+            log.println("Array size validation complete(finally block)\n");
         }
     }
 
@@ -62,7 +64,7 @@ public class Exc {
         }
     }
 
-    static void nestedFileReadDemo() {
+    static void nestedFileReadDemo(PrintWriter log) {
         System.out.println("Nested Try-Catch File Reading Demo:");
         File file = new File("numbers.txt");
         Scanner sc = null;
@@ -79,17 +81,21 @@ public class Exc {
                         sum += num;
                     } catch (NumberFormatException e) {
                         System.out.println("Invalid number skipped: " + token);
+                        log.println("Invalid number skipped: " + token);
                     }
                 }
                 System.out.println("Sum of valid numbers: " + sum);
+                log.println("Sum of valid numbers: " + sum);
             } 
             catch (Exception e) {
                 System.out.println("Error while reading file: " + e.getMessage());
+                log.println("Error while reading file: " + e.getMessage());
             }
 
         } 
         catch (FileNotFoundException e) {
             System.out.println("File not found: " + e.getMessage());
+            log.println("File not found: " + e.getMessage());
         } 
         finally {
             if (sc != null) sc.close();
@@ -97,11 +103,9 @@ public class Exc {
         }
     }
 
-    static double balance = 5000;
-
-    static void transfer(double amount) throws InsufficientBalanceException {
+    static void transfer(double amount, PrintWriter log, double balance) throws InsufficientBalanceException {
         try {
-            if (amount > balance) {
+            if (amount >= balance) {
                 throw new InsufficientBalanceException("Transfer failed: Insufficient balance");
             }
             balance -= amount;
@@ -109,53 +113,77 @@ public class Exc {
             return;
         } 
         finally {
-            try (FileWriter log = new FileWriter("transaction_log.txt", true)) {
-                log.write("Attempted transfer: Rs." + amount + "\n");
-            } catch (IOException e) {
-                System.out.println("Error writing log: " + e.getMessage());
-            }
-            System.out.println("Transaction attempt logged (finally block).\n");
+            log.println("Attempted transfer of amount "+amount);
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        FileWriter fw = new FileWriter("Exceptions.txt", true);
+        PrintWriter log = new PrintWriter(fw, true);
+
+        Scanner sc = new Scanner(System.in);
     
-        dynamicArrayDemo();
+        dynamicArrayDemo(sc,log);
 
         System.out.println("Voting Age Validation Demo:");
-        int[] testAges = {15, 25, 130};
+        System.out.print("Enter the number of ages to be checked: ");
+        int s = sc.nextInt();
+        int[] testAges = new int[s];
+        int a;
+
+        System.out.print("Enter the ages: ");
+        for (int i = 0; i<s; i++){
+            a = sc.nextInt();
+            testAges[i] = a;
+        }
 
         for (int age : testAges) {
             try {
                 System.out.println("Checking age: " + age);
+                log.println("Checking age: " + age);
                 checkVotingEligibility(age);
             } 
             catch (UnderageException e) {
-                System.out.println("Unchecked Exception: " + e.getMessage());
+                System.out.println("Exception: " + e.getMessage());
+                log.println("Exception: " + e.getMessage());
             } 
             catch (UnrealisticAgeException e) {
-                System.out.println("Checked Exception: " + e.getMessage());
+                System.out.println("Exception: " + e.getMessage());
+                log.println("Exception: " + e.getMessage());
             } 
             finally {
                 System.out.println("Validation completed for age: " + age + "\n");
             }
         }
 
-        nestedFileReadDemo();
+        nestedFileReadDemo(log);
 
-        System.out.println("Transaction Simulation Demo:");
-        try {
-            transfer(8000);
-        } 
-        catch (InsufficientBalanceException e) {
-            System.out.println("Exception: " + e.getMessage());
+        System.out.println("Simple Transaction:");
+        System.out.print("Enter balance: ");
+        double balance = sc.nextInt();
+        System.out.print("Enter the number of transfer amounts to be checked: ");
+        int si = sc.nextInt();
+        double[] TransferAmount = new double[s];
+        double t;
+
+        System.out.print("Enter the amounts: ");
+        for (int i = 0; i<s; i++){
+            t = sc.nextInt();
+            TransferAmount[i] = t;
         }
 
-        try {
-            transfer(3000);
-        } 
-        catch (InsufficientBalanceException e) {
-            System.out.println("Exception: " + e.getMessage());
-        }
+        for (double i : TransferAmount){
+
+        
+            try {
+                System.out.println("Transfer amount: "+i);
+                transfer(i, log, balance);
+            } 
+            catch (InsufficientBalanceException e) {
+                System.out.println("Exception: " + e.getMessage());
+                log.println("Exception: " + e.getMessage());
+            }
+    }
     }
 }
